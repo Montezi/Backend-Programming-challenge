cbcb0d220e4a516d0001a52ce4fe1ad5b4af486b
Montezi <mirianmontezi.pro@gmail.com>
2019-09-12 23:22:10 -0300

    first commit

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,47 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    const userExists = await User.findOne({ where: { email: req.body.email } });
+
+    if (userExists) {
+      return res.status(400).json({ error: 'User already exists' });
+    }
+
+    const { id, name, email, state } = await User.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      state,
+    });
+  }
+
+  async update(req, res) {
+    const { email, oldPassword } = req.body;
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists' });
+      }
+    }
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name, state } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      state,
+    });
+  }
+}
+
+export default new UserController();
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '9fde5947fb2489e080f88e050c00e3bd',
+  expiresIn: '5d',
+};
