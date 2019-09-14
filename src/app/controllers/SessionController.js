fb3fd9be2090de67c367665eaf637f4ebc81ccfb
Montezi <mirianmontezi.pro@gmail.com>
2019-09-14 17:53:36 -0300

    add cors

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,8 +1,9 @@
 import jwt from 'jsonwebtoken';
 import * as Yup from 'yup';
 
-import User from '../models/User';
 import authConfig from '../../config/auth';
+import User from '../models/User';
+import File from '../models/File';
 
 class SessionController {
   async store(req, res) {
@@ -19,7 +20,16 @@ class SessionController {
 
     const { email, password } = req.body;
 
-    const user = await User.findOne({ where: { email } });
+    const user = await User.findOne({
+      where: { email },
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
 
     if (!user) {
       return res.status(401).json({ error: 'User not found' });
@@ -29,7 +39,7 @@ class SessionController {
       return res.status(401).json({ error: 'Password does not match' });
     }
 
-    const { id, name, state } = user;
+    const { id, name, state, avatar } = user;
 
     return res.json({
       user: {
@@ -37,6 +47,7 @@ class SessionController {
         name,
         email,
         state,
+        avatar,
       },
       token: jwt.sign({ id }, authConfig.secret, {
         expiresIn: authConfig.expiresIn,
